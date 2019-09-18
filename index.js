2676a25b41c77ee39a18904dcc3cd8041d0bd84f
sallykiddo <salva7099@gmail.com>
2019-09-18 13:27:59 +0200

    Update index.js

--- index.js
+++ index.js
@@ -55,13 +55,13 @@ function refreshDeviceList(){
 
 function onDiscoverDevice(device){
 	//Make a list in html and show devises
-	if(device.name == "BORDLAMPE"){
+	//if(device.name == "BORDLAMPE"){
 		
 		var listItem = document.createElement('li'),
 		html = device.name+ "," + device.id;
 		listItem.innerHTML = html;
 		document.getElementById("bleDeviceList").appendChild(listItem);
-	}
+	//}
 }
 
 
@@ -72,7 +72,7 @@ function conn(){
 	ConnDeviceId = deviceTouchArr[1];
 	bleDeviceName = deviceTouchArr[0];
 	document.getElementById("debugDiv").innerHTML += "<br>Debug: <br>"+deviceTouchArr[0]+"<br>"+deviceTouchArr[1]; //for debug:
-	if(bleDeviceName  == "BORDLAMPE")
+	if(bleDeviceName  == "BORDLAMPE" )
 		
 		test();
  }
